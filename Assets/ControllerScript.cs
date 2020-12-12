b8b833e608f093bd3755d393ee376eb26d242109
nope0124 <kami-ryo0124@ezweb.ne.jp>
2020-12-13 05:46:39 +0900

    First

---
+++ Assets/ControllerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ControllerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void optionButton(){
+        SceneManager.LoadScene("Option");
+    }
+    
+    public void startButton(){
+        SceneManager.LoadScene("Main");
+    }
+}
